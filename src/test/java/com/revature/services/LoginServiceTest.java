233566cd15d8fe143b9ae7e8ab81f94bc5a26f9b
Oleksander <sanya953303@gmail.com>
2020-12-21 20:58:19 -0800

    add 1 test

--- src/test/java/com/revature/services/LoginServiceTest.java
+++ src/test/java/com/revature/services/LoginServiceTest.java
@@ -1,5 +1,16 @@
 package com.revature.services;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
 public class LoginServiceTest {
 
+	public static LoginService ls = new LoginService();
+	
+	@Test public void getLoginWithCorrectData1() {
+		assertTrue(ls.login("employee", "pass"));
+	}
+	
+	
 }
